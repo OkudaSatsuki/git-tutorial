8104bca6228d4cb62b4a7ca8ae690ef8026ac2de
root <root@j1610.epi.it.matsue-ct.jp>
2019-10-02 11:46:20 +0900

    プログラム完成

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+for i in 1..100 do
+	if (i%3==0 && i%5==0) then
+		puts("fizzbuzz\n")
+	elsif (i%3==0) then
+		puts("fizz\n")
+	elsif (i%5==0) then
+		puts("buzz\n")
+	else 
+		puts(i)
+	end
+end
